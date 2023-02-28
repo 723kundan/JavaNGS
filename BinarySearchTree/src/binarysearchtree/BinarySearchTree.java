d13cae41f76159cfd4ab7a74b41de9125e93e52a
Kundan Kumar Jha <kkjmdu27@gmail.com>
2023-02-28 16:35:50 +0530

    first commit

---
+++ BinarySearchTree/src/binarysearchtree/BinarySearchTree.java
@@ -0,0 +1,56 @@
+
+package binarysearchtree;
+
+class Node{
+    char key;
+    Node left,right;
+    
+    Node(char key){
+        this.key=key;
+    }
+}
+class Bst{
+    Node root;
+    void insertKey(char key){
+        root = insertInTree(root,key);
+    }
+    
+    Node insertInTree(Node root,char key){
+        if(root == null){
+            root = new Node(key);
+            return root;
+        }
+        if(key<root.key) {
+            root.left = insertInTree(root.left, key);
+        }
+        else if(key>root.key){
+            root.right = insertInTree(root.right,key);
+        }
+        return root;
+    }
+    void preordertraversal(Node root) {
+        if(root !=null){
+            System.out.print(root.key+" ");
+            preordertraversal(root.left);
+            preordertraversal(root.right);
+        }
+    }
+}
+
+public class BinarySearchTree {
+
+  
+    public static void main(String[] args) {
+        
+        Bst t1 = new Bst();
+        t1.insertKey('C');
+        t1.insertKey('I');
+        t1.insertKey('B');
+        t1.insertKey('G');
+        t1.insertKey('M');
+        t1.insertKey('A');
+        
+        t1.preordertraversal(t1.root);
+    }
+    
+}
